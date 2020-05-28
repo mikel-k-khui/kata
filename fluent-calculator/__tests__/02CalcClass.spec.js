7769c40a1283291ad8c139fe22482e8d576757ad
Mike Chui <mikel.k.khui@gmail.com>
2020-05-28 11:54:38 -0600

    config linter/prettier

--- fluent-calculator/__tests__/02CalcClass.spec.js
+++ fluent-calculator/__tests__/02CalcClass.spec.js
@@ -1,19 +1,17 @@
-const Calcalutor = require('../src/Calc');
+const Calcalutor = require('../src/Calc')
 
 // describe the behaviours of the Calc class
 describe('create Calc class', () => {
-  
-  // test whether you can create the class
-  // test('class exists and was called', () => {
-  //   const Calc = new Calcalutor();
-  //   expect(Calc.foo).toBe(undefined);
-  // })
+    // test whether you can create the class
+    test('class exists and was called', () => {
+        const Calc = new Calcalutor()
+        expect(typeof Calc.add).toBe('function')
+    })
 
-  // test whether you can call a method
-  test('methods exists', () => {
-    const Calc = new Calcalutor();
-    console.log(Object.getOwnPropertyNames(Calc))
-    expect(Calc.new().zero().add()).toBe('bar');
-  })
-  
+    // test whether you can call a method
+    test('methods exists', () => {
+        const Calc = new Calcalutor()
+        console.log(Object.getOwnPropertyNames(Calc))
+        expect(Calc.new().zero().add()).toBe('bar')
+    })
 })
--- fluent-calculator/src/Calc.js
+++ fluent-calculator/src/Calc.js
@@ -1,38 +1,46 @@
-const operators = [ 'add' ];
-const values = ['zero', 'one'];
+// define constants
+const operators = {
+    plus: (x, y) => Math.round(x + y),
+    minus: (x, y) => Math.round(x - y),
+}
+
+const values = ['zero', 'one']
 
 module.exports = class Calcalutor {
+    /**
+     * Adds two numbers together.
+     * @param {int} num1 The first number.
+     * @param {int} num2 The second number.
+     */
+    constructor() {
+        this._foo = undefined
+        this.defineMethod()
+    }
 
-  constructor() {
-    this._foo = undefined;
-    this.define_method();
-  }
+    /*
+     * Goal is to mimic the define_method function in Ruby to create an array of functions
+     * This function loops through the arrays of acceptable function names
+     * and assign them the same process
+     */
+    static defineMethod() {
+        this['new'] = this.bypass
+        operators.forEach((operator) => (this[operator] = this.process))
+        values.forEach((value) => (this[value] = this.process))
+    }
 
-  /* 
-  * Goal is to mimic the define_method function in Ruby to create an array of functions
-  * This function loops through the arrays of acceptable function names
-  * and assign them the same process
-  */
-  define_method () {
-    this['new'] = this.bypass;
-    operators.forEach(operator => this[operator] = this.process);
-    values.forEach(value => this[value] = this.process);
-  }
+    /*
+     * Evaluate each call through the fluent syntax to assess
+     */
+    process() {
+        return !this._foo ? this.setFoo() : this._foo
+    }
 
-  /*
-  * Evaluate each call through the fluent syntax to assess 
-  */
-  process() {
-    return (!this._foo ? this.setFoo() : this._foo);
-  }
-  
-  bypass() {
-    return this
-  }
+    bypass() {
+        return this
+    }
 
-  setFoo() {
-    this._foo = 'bar';
-    return this;
-  }
-
+    setFoo() {
+        this._foo = 'bar'
+        return this
+    }
 }
