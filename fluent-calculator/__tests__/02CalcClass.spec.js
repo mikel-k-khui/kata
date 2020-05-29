ebe6079ecc418c22ad942178b65a18734814dab2
Mike Chui <mikel.k.khui@gmail.com>
2020-05-29 13:05:28 -0600

    Refactor: update tests for getter functions, added getter

--- fluent-calculator/__tests__/02CalcClass.spec.js
+++ fluent-calculator/__tests__/02CalcClass.spec.js
@@ -8,32 +8,21 @@ describe('create Calc class', () => {
     expect(typeof Calc.plus).toBe('object')
   })
 
-  // test methods that do not exists
-  test('methods have the incorrect type and should throw TypeError', () => {
-    const Calc = new Calcalutor()
-    expect(() => Calc.new()).toThrow(TypeError)
-    expect(() => Calc.new.twelve()).toThrow(TypeError)
-    expect(() => Calc.new.five().plus()).toThrow(TypeError)
-    expect(() => Calc.new.five().plus.fifteen()).toThrow(TypeError)
-  })
-
-  // test methods that do not exists
+  // test methods that do not existss
   test('methods do not exist and returns undefined', () => {
     const Calc = new Calcalutor()
     expect(typeof Calc.twelve).toBe('undefined')
     expect(typeof Calc.new.twelve).toBe('undefined')
-    expect(typeof Calc.new.five().add).toBe('undefined')
-    expect(typeof Calc.new.five().plus.fifteen).toBe('undefined')
+    expect(typeof Calc.new.five.add).toBe('undefined')
+    expect(typeof Calc.new.five.plus.twelve).toBe('undefined')
   })
 
   // test whether you can call a method
   test('each methods exists and returns appropriate type', () => {
     const Calc = new Calcalutor()
     expect(typeof Calc.new).toBe('object')
-    expect(typeof Calc.new.five).toBe('function')
-    expect(typeof Calc.new.five()).toBe('object')
-    expect(typeof Calc.new.five().plus).toBe('object')
-    expect(typeof Calc.new.five().plus.five).toBe('function')
-    expect(typeof Calc.new.five().plus.five()).toBe('number')
+    expect(typeof Calc.new.five).toBe('object')
+    expect(typeof Calc.new.five.plus).toBe('object')
+    expect(typeof Calc.new.five.plus.five).toBe('number')
   })
 })
