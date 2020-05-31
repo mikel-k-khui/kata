cbe99073e61b5acc65fb1319a880e5d17967ab58
Mike Chui <mikel.k.khui@gmail.com>
2020-05-30 23:25:35 -0600

    refactor tile and decoder.

--- secret-string/__tests__/01Decoder.spec.js
+++ secret-string/__tests__/01Decoder.spec.js
@@ -10,7 +10,7 @@ describe('create Decoder class', () => {
   })
 
   // test whether you can call a method
-  test('each methods exists and returns appropriate type or value', () => {
+  test('each methods (exclude, decodeAll) exists and returns appropriate type or value', () => {
     const decoder = new Decoder()
     expect(decoder.isEmpty()).toEqual(true)
 
@@ -35,13 +35,13 @@ describe('create Decoder class', () => {
     expect(decoder.isEmpty()).toEqual(false)
     expect(decoder.tiles).toEqual([testTileA, testTileB, testTileC, testTileD])
 
-    // remove a tile in 1 links
+    // remove a tile with 1 link
     decoder.removeTile(decoder.tiles, testTileD)
     testTileC.removeLink(testTileD.letter) // remove 'd' from c.links
     expect(decoder.tiles).toEqual([testTileA, testTileB, testTileC])
     expect(decoder.getNullTile()).toEqual(testTileC)
 
-    // remove a tile in 2 links
+    // remove a tile with 2 links
     decoder.removeTile(decoder.tiles, testTileC)
     testTileA.removeLink(testTileC.letter) // remove 'c' from a.links
     testTileB.removeLink(testTileC.letter) // remove 'c' from b.links
@@ -53,15 +53,28 @@ describe('create Decoder class', () => {
     expect(decoder.isEmpty()).toEqual(true)
   })
 
-  // test input errors
-  test('incorrect inputs returns appropriate error', () => {
+  // test input errors and messages
+  test('incorrect inputs returns appropriate error and/or message', () => {
     const decoder = new Decoder()
 
     // add incorrect hint to decoder
     expect(() => decoder.decodeTriplet([NaN, 'a'])).toThrow(TypeError)
     expect(decoder.isEmpty()).toEqual(true)
 
+    // update tiles without an array
+    expect(() => (decoder.tiles = 'a')).toThrow(TypeError)
+    expect(decoder.isEmpty()).toEqual(true)
+
     // remove non-existent
     decoder.decodeTriplet(['a', 'b', 'c'])
+    const testTileD = new Tile('d')
+    const spy = jest.spyOn(console, 'assert')
+    expect(decoder.removeTile(decoder.tiles, testTileD)).toBe(undefined)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  // test whether you can call a method
+  it('should take an array of hints to revreal the message', () => {
+    console.log('holding place')
   })
 })
--- secret-string/src/tile.js
+++ secret-string/src/tile.js
@@ -1,7 +1,20 @@
 /**
- * Use directed graph links create
+ * Goal is to take in hints in order triplets letters then reveal the secret
+ *
+ * The secret is revealed by apply directed graph to work backwards:
+ * - loop through all hints to create array of tiles.
+ * - tiles contains letter and linked tiles by order of appearance.
+ * - It starts with the tile (think scrabble) with no linked tiles.
+ * - Append it to the message like a Queue (i.e. FIFO).
+ * - Remove the tile and tile's letter from remaining tiles' links.
+ * - complete this until no more tile is left
+ *
+ * Basic structure is:
+ * tiles - collection of tile
+ * tile class - letter of the tile, any tiles linked (i.e. appear after) it
+ *
+ * Tile class is responsible for a tile's creation and CUD of links tile letters
  */
-
 module.exports = class Tile {
   constructor(letter, letterAfter = undefined) {
     this._letter = letter
@@ -50,13 +63,16 @@ module.exports = class Tile {
 
   removeLink(link) {
     const position = this.links.indexOf(link)
+    console.assert(
+      position !== -1,
+      `${link} cannot be found in links array ${this.links}`
+    )
+
     if (position !== -1) {
       const currentLinks = [...this.links]
       this.links = currentLinks
         .slice(0, position)
         .concat(currentLinks.slice(position + 1))
-    } else {
-      console.log(link, ' cannot be found in links array', this)
     }
   }
 }
